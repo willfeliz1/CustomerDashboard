27cb155addbee3e45151f32f4514f51539b14b35
willfeliz1 <william_mfelizardo@hotmail.com>
2020-10-28 23:00:49 -0300

    feat: buttonTex is optional with undefined as defaultProps

--- src/components/ButtonSpanStrong/index.tsx
+++ src/components/ButtonSpanStrong/index.tsx
@@ -3,18 +3,22 @@ import React from 'react';
 import { Container, Button } from './styles';
 
 interface IButtonSpanStrong {
-  buttonText: string;
+  buttonText?: number;
   buttonColor: string;
   title: string;
   description: string;
 }
 
 const ButtonSpanStrong: React.FC<IButtonSpanStrong> = ({
-  buttonText = '',
+  buttonText,
   buttonColor,
   title,
   description,
 }: IButtonSpanStrong) => {
+  ButtonSpanStrong.defaultProps = {
+    buttonText: undefined,
+  };
+
   return (
     <Container>
       <Button type="button" buttonColor={buttonColor}>
