9e007e4b210369b4f81891bab427909da9c4235e
willfeliz1 <william_mfelizardo@hotmail.com>
2021-01-04 19:20:15 -0300

    refactor: file renamed

---
+++ src/components/TransitionTooltip/TransitionTooltip.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import { Zoom } from '@material-ui/core';
+import { Info } from '@material-ui/icons';
+
+interface ITitle {
+  texto: string;
+}
+
+const ToolTipInfo: React.FC<ITitle> = ({ texto }: ITitle) => {
+  return (
+    <div>
+      <Tooltip TransitionComponent={Zoom} title={texto} placement="top" arrow>
+        <Info />
+      </Tooltip>
+    </div>
+  );
+};
+
+export default ToolTipInfo;
